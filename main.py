e1b84bdb4250584ffd3925485b3010c90ab41206
Romain <justromixx@gmail.com>
2024-06-04 08:48:20 +0200

    Update main.py
    - refactor code in order to make it easier
    - added numpy library to remove complicated and deep copy arrays

--- main.py
+++ main.py
@@ -1,25 +1,8 @@
 import math
+import numpy as np
 
 taille_grille_x = 20
 taille_grille_y = 20
-
-def ajouter_stable(grille):
-    
-    stable = [
-        [1, 1, 1, 0],
-        [0, 0, 1, 1],
-        [0, 0, 0, 1],
-    ]
-
-    osi = [
-       [1, 1, 1, 0],
-        [0, 0, 1, 0],
-        [0, 1, 0, 0],
-    ]
-
-    for x in range(0, len(osi)) :
-        for y in range(0, len(osi[0])) :
-           grille[math.floor(taille_grille_x / 2)+ x][math.floor(taille_grille_y / 2) + y] = osi[x][y]
 
 def print_state(grid) :
     for x in range(0, taille_grille_x) :
@@ -29,6 +12,18 @@ def print_state(grid) :
             else :
                 print('□', end=' ')
         print('')
+
+def ajouter_stable(grille):
+    
+    patern = [
+        [1],
+        [1],
+        [1],
+    ]
+
+    for x in range(0, len(patern)) :
+        for y in range(0, len(patern[x])) :
+           grille[math.floor(taille_grille_x / 2) + x][math.floor(taille_grille_y / 2) + y] = patern[x][y]
 
 def get_neighbord_count(grid, targetX, targetY):
     count = 0
@@ -50,32 +45,30 @@ def get_neighbord_count(grid, targetX, targetY):
 
 def calculate_next_state(grid, next_grid):
     for x in range(0, taille_grille_x) :
-            for y in range(0, taille_grille_y) :
-                cell = grid[x][y]
-                isAlive = cell == 1
-                neightbord_count = get_neighbord_count(grid, x, y) 
-                if (isAlive) :
-                    if (neightbord_count < 2 or neightbord_count > 3) :
-                        next_grid[x][y] = 0
-                    else :
-                        next_grid[x][y] = 1
+        for y in range(0, taille_grille_y) :
+            neightbord_count = get_neighbord_count(grid, x, y) 
+            if (grid[x][y] == 1) :
+                if (neightbord_count < 2 or neightbord_count > 3) :
+                     next_grid[x][y] = 0
                 else :
-                    if (neightbord_count == 3):
-                        next_grid[x][y] = 1
-                    else:
-                        next_grid[x][y] = 0
+                    next_grid[x][y] = 1
+            else :
+                if (neightbord_count == 3):
+                    next_grid[x][y] = 1
+                else:
+                    next_grid[x][y] = 0
 
 def loop() :
     tours = 10
     i = 0
-    grille = [[0 for y in range(taille_grille_y)] for x in range(taille_grille_x)]
-    next_grille = [[0 for y in range(taille_grille_y)] for x in range(taille_grille_x)]
+    grille = np.zeros((taille_grille_x, taille_grille_y), dtype=int)
+    next_grille = np.zeros((taille_grille_x, taille_grille_y), dtype=int)
     ajouter_stable(grille)
 
     while(i < tours) :
         print_state(grille)
         calculate_next_state(grille, next_grille)
-        grille = [row[:] for row in next_grille]
+        grille = np.copy(next_grille)
         print('')
         i += 1
 
